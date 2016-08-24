b77b3a0986a89f9c0121cd7d2b42ccc8e3abdef8
root <root@sausageland.is.cc>
2016-08-24 15:11:04 -0400

    removed some junk

--- ctf/settings.py
+++ ctf/settings.py
@@ -53,8 +53,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ctf',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        #'USER': 'ctfman',
-        #'PASSWORD': '1*k2F2DC=CD*Oz!z%qB*',
         'USER': 'stu',
         'PASSWORD': 'nope',
         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
